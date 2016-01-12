854612e12cebf2088b166d3403d98eb178679c9a
Rafal Nater <naterek@gmail.com>
2016-01-12 19:00:36 +0100

    Add about page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  layout 'app2', only: [:about, :contact]
+
   def home
   	@posts = Post.take(3)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get 'contact' => 'static_pages#contact'
 
   resources :posts
-  root "posts#index"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
