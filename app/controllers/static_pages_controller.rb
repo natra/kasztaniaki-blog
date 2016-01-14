e11f4c74c2e738d051e1030d702b1839effc5358
Rafal Nater <naterek@gmail.com>
2016-01-15 00:44:02 +0100

    Make team table load values directly from database

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   layout 'admin', only: [:admin]
 
   def home
+    @team_selections = TeamSelection.all
   	@posts = Post.take(3)
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,46 @@
 module ApplicationHelper
+	def set_team_line(players)
+		line = ""
+		i = 0
+		case players.count
+		when 2
+			players.each do |player|
+				line += "<td></td>"
+				line += "<td>#{image_tag('shirt2.png')}<span>#{player.name}</span></td>"
+			end
+			line += "<td></td>"
+		when 3
+			if players.first.position == "3"
+				players.each do |player|
+					if (i == 1 || i == 2)
+						line += "<td></td>"
+					end
+					line += "<td>#{image_tag('shirt2.png')}<span>#{player.name}</span></td>"
+					i+=1
+				end
+			else
+				players.each do |player|
+					if i == 0
+						line += "<td></td>"
+					end
+					line += "<td>#{image_tag('shirt2.png')} <span>#{player.name}</span></td>"
+					i+=1
+				end
+				line += "<td></td>"
+			end
+		when 4
+			players.each do |player|
+				if i == 2
+					line += "<td></td>"
+				end
+				line += "<td>#{image_tag('shirt2.png')}<span>#{player.name}</span></td>"
+				i+=1
+			end
+		when 5
+			players.each do |player|
+				line += "<td>#{image_tag('shirt2.png')}<span>#{player.name}</span></td>"
+			end
+		end		
+		line.html_safe
+	end
 end
