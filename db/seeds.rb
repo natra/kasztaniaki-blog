23046494694e34e3a4cb5502f32bad67dd869008
Rafal Nater <naterek@gmail.com>
2016-01-14 23:50:57 +0100

    Populate the club and team selecion tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Club.create(name: 'Arsenal', abbrev: 'AFC')
+Club.create(name: 'Aston Villa', abbrev: 'AVL')
+Club.create(name: 'Chelsea', abbrev: 'CHE')
+Club.create(name: 'Liverpool', abbrev: 'LFC')
+Club.create(name: 'Leicester', abbrev: 'LEI')
+Club.create(name: 'Stoke', abbrev: 'STK')
+
+
+TeamSelection.create(name: 'Butland', club_id: '5', position: '0')
+
+TeamSelection.create(name: 'Zouma', club_id: '2', position: '1')
+TeamSelection.create(name: 'Zouma', club_id: '2', position: '1')
+TeamSelection.create(name: 'Zouma', club_id: '2', position: '1')
+
+TeamSelection.create(name: 'Ozil', club_id: '0', position: '2')
+TeamSelection.create(name: 'Ozil', club_id: '0', position: '2')
+TeamSelection.create(name: 'Ozil', club_id: '0', position: '2')
+TeamSelection.create(name: 'Ozil', club_id: '0', position: '2')
+
+
+TeamSelection.create(name: 'Vardy', club_id: '4', position: '3')
+TeamSelection.create(name: 'Vardy', club_id: '4', position: '3')
+
+TeamSelection.create(name: 'Moreno', club_id: '3', position: '4')
+TeamSelection.create(name: 'Moreno', club_id: '3', position: '4')
+TeamSelection.create(name: 'Moreno', club_id: '3', position: '4')
+TeamSelection.create(name: 'Moreno', club_id: '3', position: '4')
+
